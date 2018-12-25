ec43af76b1062b6782318edbc8cbcf2ae671f920
Artem Verbat <artem.verbat@gmail.com>
2018-12-25 05:53:35 +0200

    Use formik and yup instead a redux-form for task creation form.

--- src/components/CreationForm.js
+++ src/components/CreationForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { InputField } from 'react-semantic-redux-form';
-import { Form, Field, reduxForm } from 'redux-form';
+import { withFormik } from 'formik';
+import { Form, Input, Button } from 'semantic-ui-react';
 import { addTask } from '../actions/index';
+import * as yup from "yup";
 
 @connect(state => ({
   tasksCounter: state.tasks.taskList.length + 1
@@ -10,36 +11,69 @@ import { addTask } from '../actions/index';
   addTask
 })
 
-@reduxForm({
-  form: 'addTask'
+@withFormik({
+  mapPropsToValues: () => ({
+    name: ''
+  }),
+
+  validationSchema: yup.object().shape({
+    name: yup.string().required('Task name is required')
+  }),
+
+  handleSubmit: (values, { props, resetForm }) => {
+    setTimeout(() => {
+      props.addTask({
+        id: Date.now(),
+        ...values
+      });
+
+      resetForm();
+    }, 1000);
+  }
 })
 
 class CreationForm extends Component {
+  componentDidMount() {
+    this.props.validateForm();
+  }
+
   render() {
-    const { tasksCounter, handleSubmit } = this.props;
+    const {
+      values,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      isSubmitting,
+      isValid,
+      tasksCounter
+    } = this.props;
 
     return (
-      <Form name="addTask" onSubmit={ handleSubmit(this.handleSubmit) }>
-        <Field
-          component={InputField}
-          name="name"
+      <Form onSubmit={handleSubmit} noValidate loading={isSubmitting}>
+        <Input
+          name='name'
+          value={values.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          disabled={isSubmitting}
           fluid
+          action
           focus
-          action={{ color: 'teal', content: `Add #${tasksCounter}` }}
           placeholder='Enter task name...'
-        />
+          required
+        >
+          <input />
+          <Button
+            type='submit'
+            color='teal'
+            disabled={!isValid}
+          >
+            Add #{tasksCounter}
+          </Button>
+        </Input>
       </Form>
     );
   }
-
-  handleSubmit = (fields) => {
-    this.props.addTask({
-      id: Date.now(),
-      name: fields.name
-    });
-
-    this.props.reset();
-  };
 }
 
 export default CreationForm;
